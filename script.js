dc03cdbe389514345b0b4f81e2765032174805c3
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-09 07:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,30 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+    const pricesElement=document.querySelectorAll(".price");
+
+    const sum=
+    Array.from(pricesElement).reduce((s,ele)=>{
+        s+=Number(ele.textContent);
+        return s;
+    },0);
+
+    const newRow=document.createElement("tr");
+
+    const td1=document.createElement("td");
+    td1.textContent="Total";
+
+    const td2=document.createElement("td");
+    td2.textContent=sum;
+
+    newRow.append(td1,td2);
+
+    document.querySelector("table").appendChild(newRow);
+
+    getSumBtn.style.display="none";
   
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
